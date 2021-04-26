fab4909bdd1690c419a959300c87784a3dd7784e
ChrisBakaloff <chrisbklv@gmail.com>
2021-04-26 14:17:46 +0100

    Initial commit

---
+++ BAKANET/MainPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainPageViewController.swift
+//  BAKANET
+//
+//  Created by Hristiyan Bakalov on 26/04/2021.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BAKANET/ViewController.swift
+++ BAKANET/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var LoginButton: UIButton!
+    @IBOutlet weak var LogoLabel: UILabel!
+    @IBOutlet weak var RegisterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        LoginButton.layer.cornerRadius = 20
+//        RegisterButton.layer.cornerRadius = 20
+//        LogoLabel.layer.cornerRadius = 20
+//        LoginButton.layer.masksToBounds = true
+//        RegisterButton.layer.masksToBounds = true
+//        LogoLabel.layer.masksToBounds = true
+
+        
+//        LoginButton.backgroundColor = UIColor.white.withAlphaComponent(0.4)
+//        LogoLabel.backgroundColor = UIColor.white.withAlphaComponent(0.4)
+//        RegisterButton.backgroundColor = UIColor.white.withAlphaComponent(0.4)
+        
     }
 
 
